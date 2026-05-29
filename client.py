8a09808394986da180ebb5431dce674d51ceef43
Shravan <shravankandakumar3580@gmail.com>
2026-05-29 23:14:17 +0530

    Recent save... Added error Handling (little Bit)

--- client.py
+++ client.py
@@ -7,42 +7,59 @@ window.title("FlaskControlPanel")
 window.geometry('500x500')
 
 def sendForward():
-    data = {"com": "SFCforward"} #SFC means Sent From Client :)
-    comms = requests.post(url, json=data)
-    recvmssg = comms.json()
-    actualMssg = recvmssg["response"]
-    print(actualMssg)
-
+    try:
+        data = {"com": "SFCforward"} #SFC means Sent From Client :)
+        comms = requests.post(url, json=data)
+        recvmssg = comms.json()
+        actualMssg = recvmssg["response"]
+        responseLabel.config(text=actualMssg)
+        
+    except:
+        responseLabel.config(text="the json Data did not get sent (SFCforward)")
 def sendBackward():
-    data = {"com": "SFCbackward"} #SFC means Sent From Client :)
-    comms = requests.post(url, json=data)
-    recvmssg = comms.json()
-    actualMssg = recvmssg["response"]
-    print(actualMssg)
+    try:
+        data = {"com": "SFCbackward"} #SFC means Sent From Client :)
+        comms = requests.post(url, json=data)
+        recvmssg = comms.json()
+        actualMssg = recvmssg["response"]
+        responseLabel.config(text=actualMssg)
+
+    except:
+        responseLabel.config(text="the json Data did not get sent (SFCbackward)")
 
 def sendLeft():
-    data = {"com": "SFCleft"} #SFC means Sent From Client :)
-    comms = requests.post(url, json=data)
-    recvmssg = comms.json()
-    actualMssg = recvmssg["response"]
-    print(actualMssg)
+    try:
+        data = {"com": "SFCleft"} #SFC means Sent From Client :)
+        comms = requests.post(url, json=data)
+        recvmssg = comms.json()
+        actualMssg = recvmssg["response"]
+        responseLabel.config(text=actualMssg)
+
+    except:
+        responseLabel.config(text="the json Data did not get sent (SFCleft)")
 
 def sendRight():
-    data = {"com": "SFCright"} #SFC means Sent From Client :)
-    comms = requests.post(url, json=data)
-    recvmssg = comms.json()
-    actualMssg = recvmssg["response"]
-    print(actualMssg)
+    try:
+        data = {"com": "SFCright"} #SFC means Sent From Client :)
+        comms = requests.post(url, json=data)
+        recvmssg = comms.json()
+        actualMssg = recvmssg["response"]
+        responseLabel.config(text=actualMssg)
+
+    except:
+        responseLabel.config(text="the json Data did not get sent (SFCright)")
 
 
 forwardButton = tk.Button(window, text="Forward", command=lambda:sendForward())
 backwardButton = tk.Button(window, text="Backward", command=lambda:sendBackward())
 leftButton = tk.Button(window, text="Left", command=lambda:sendLeft())
 rightButton = tk.Button(window, text="right", command=lambda:sendRight())
+responseLabel = tk.Label(window, text="!@#YET TO RECEIVE#@!")
 
-forwardButton.grid(row=0, column=1, padx=5, pady=5)
-leftButton.grid(row=1, column=0, padx=5, pady=5)
-rightButton.grid(row=1, column=2, padx=5, pady=5)
-backwardButton.grid(row=4, column=1, padx=5, pady=5)
+forwardButton.grid(row=0, column=1)
+leftButton.grid(row=1, column=0)
+rightButton.grid(row=1, column=2)
+backwardButton.grid(row=4, column=1)
+responseLabel.grid(row=6, column=1)
 
 window.mainloop()
--- raspiCode.py
+++ raspiCode.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
 import time
 import serial
+import threading
 port = ''
 ard = serial.Serial(port, 9600)
+USvalue = 0;
 
 app = Flask(__name__)
+
+def getUSvalue():
+    USvalue = ard.read_until('\n')
+
+threading.Thread(target=getUSvalue).start()
 
 @app.route('/dataSending', methods=["POST"])
 def dataStuff():
@@ -13,19 +20,19 @@ def dataStuff():
 
     try:
         if(command == 'SFCforward'):
-            ard.write(b'F')
+            ard.write(b'F\n')
             return jsonify({"response": "Command Sent"})
 
-        if(command == 'SFCbackward'):
-            ard.write(b'B')
+        elif(command == 'SFCbackward'):
+            ard.write(b'B\n')
             return jsonify({"response": "Command Sent"})
 
-        if(command == 'SFCleft'):
-            ard.write(b'L')
+        elif(command == 'SFCleft'):
+            ard.write(b'L\n')
             return jsonify({"response": "Command Sent"})
 
-        if(command == 'SFCright'):
-            ard.write(b'R')
+        elif(command == 'SFCright'):
+            ard.write(b'R\n')
             return jsonify({"response": "Command Sent"})
 
     except:
