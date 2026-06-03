190c3e8927c0db3d27de763dd3f2a80cdf2b578a
Shravan <shravankandakumar3580@gmail.com>
2026-06-03 23:43:24 +0530

    Added Camera Features...

--- raspiCode.py
+++ raspiCode.py
@@ -1,12 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import time
 import serial
 import threading
+import cv2
 
+webcam = cv2.VideoCapture('/dev/video0')
+webcam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 port = '/dev/ttyACM0' #The port at which the Arduino is Connected to on the Raspberry PI...
 ard = serial.Serial(port, 9600)
-
 app = Flask(__name__)
+
+def generate_frames():
+    while True:
+        ret, frame = webcam.read()
+        frame = cv2.flip(frame, 1)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+
+        yield(
+             b'--frame\r\n'
+             b'Content-Type: image/jpeg\r\n\r\n'
+             + frame_bytes +
+            b'\r\n'
+        )
 
 @app.route('/dataSending', methods=["POST"])
 def dataStuff():
@@ -38,7 +54,15 @@ def dataStuff():
         print("Error Dude")
         return jsonify({"response": "An Error Has Occured"})
 
+@app.route('/stream')
+def video():
+    ret, frame = webcam.read()
 
-        
+    if ret:
+        return Response(
+            generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame'
+        )
+    elif not ret:
+        return "No Camera was Detected"
 
 app.run(host='0.0.0.0', port=5000)
